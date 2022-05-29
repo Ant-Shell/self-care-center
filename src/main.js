e917bab5e53057cf655644c06f5fdea14c3e34b2
Anthony Shellman <atshellman@gmail.com>
2022-05-29 13:52:39 -0600

    Add various functions, listener, variables and html for message functionality

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 // Button Related Selectors
-var mantraRadio = document.querySelector('#radio1');
+var affirmationRadio = document.getElementById('radio1');
 
-var affimrationRadio = document.querySelector('#radio2');
+var mantraRadio = document.getElementById('radio2');
 
-var receiveMessageButton = document.querySelector('#message-button');
+var receiveMessageButton = document.getElementById('message-button');
 
-var meditationImage = document.getElementById('meditation-image');
+var meditationImage = document.getElementById('meditation-img');
+
+// var imageBox = document.getElementById('image-box')
+
+var presentMessage = document.querySelector('.present-message')
+
+var isAffirmRadio = false;
+
+var ismantraRadio = false;
+
 
 
 // Event Listeners
-
+affirmationRadio.addEventListener('click', setAffirmRadio)
+mantraRadio.addEventListener('click', setMantraRadio)
+receiveMessageButton.addEventListener('click', showRandomMessage)
 
 
 // Functions
@@ -17,24 +28,65 @@ var meditationImage = document.getElementById('meditation-image');
   function getRandomIndex(array) {
       return Math.floor(Math.random() * array.length);
   };
-  function showRandomAffirmation() {
-    var affirmation = affirmations[getRandomIndex(affirmations)];
-    // need to hide meditation image
-    hideImage();
-    // and present random affrimation in the image window
-  }
 
-  function showRandomMantra() {
+  function showRandomMessage() {
+    var affirmation = affirmations[getRandomIndex(affirmations)];
     var mantra = mantras[getRandomIndex(mantras)];
-    // need to hide meditation image
     hideImage();
-    // and present random mantra in the image window
-  }
+    // setAffirmRadio();
+    if (isAffirmRadio !== true) {
+      // displayMessage(mantra)
+      console.log(mantra)
+      } else {
+      // displayMessage(affirmation)
+      console.log(affirmation)
+      }
+    }
+
+    function setAffirmRadio() {
+      isAffirmRadio = true;
+      console.log(`isAffirmRadio Set to ${isAffirmRadio}`)
+      return isAffirmRadio;
+    }
+
+    function setMantraRadio() {
+      isMantraRadio = true;
+      console.log(`isMantraRadio Set to ${isMantraRadio}`)
+      return isMantraRadio;
+    }
+
+  // function showRandomAffirmation() {
+  //   var affirmation = affirmations[getRandomIndex(affirmations)];
+  //   // need to hide meditation image
+  //   hideImage();
+  //   // and present random affrimation in the image window
+  //   displayMessage(affirmation)
+  // }
+  //
+  // function showRandomMantra() {
+  //   var mantra = mantras[getRandomIndex(mantras)];
+  //   // need to hide meditation image
+  //   hideImage();
+  //   // and present random mantra in the image window
+  //   displayMessage(message)
+  //
+  // }
 
   function hideImage() {
-    meditationImage.classList.add(hide);
+    meditationImage.classList.add('hide');
   }
 
   function restoreImage() {
-    meditationImage.classList.remove(hide);
+    meditationImage.classList.remove('hide');
+  }
+
+  function displayMessage(message) {
+    presentMessage.innerHTML = "";
+    for (var i =0; message.length; i++) {
+      presentMessage.innerHTML +=
+      `<div>
+      <h2 class="message">${message[i]}</h2>
+      </div>
+      `
+    }
   }
